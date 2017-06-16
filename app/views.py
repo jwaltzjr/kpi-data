8066fca787c8b5d30b5d8c7365dfccb786e926f1
jwaltzjr <jwaltzjr@hotmail.com>
2017-06-16 12:27:41 -0500

    Add form handling to index

--- app/views.py
+++ app/views.py
@@ -1,11 +1,20 @@
-from flask import render_template
+from flask import render_template, url_for, flash, redirect
 
 from app import app
-from .forms import InputForm
+from . import forms, models
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    form = InputForm()
+    form = forms.InputForm()
+    if form.validate_on_submit():
+        month_data = models.MonthData(
+            form.month.data,
+            form.year.data,
+            form.miles.data,
+            form.avg_drivers.data
+        )
+        flash('Month {} has been saved.'.format(month_data))
+        return redirect(url_for('index'))
     return render_template(
         'index.html',
         title = 'KPI Data Entry',
