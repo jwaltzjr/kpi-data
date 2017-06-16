008a0fbd953a5517070420a8704f73382aac4a74
jwaltzjr <jwaltzjr@hotmail.com>
2017-06-16 10:46:28 -0500

    Add import form

---
+++ app/forms.py
@@ -0,0 +1,39 @@
+import datetime
+from flask_wtf import FlaskForm
+from wtforms import DateTimeField, IntegerField
+from wtforms.validators import DataRequired
+
+MONTHS = [
+    (1, 'January'),
+    (2, 'February'),
+    (3, 'March'),
+    (4, 'April'),
+    (5, 'May'),
+    (6, 'June'),
+    (7, 'July'),
+    (8, 'August'),
+    (9, 'September'),
+    (10, 'October'),
+    (11, 'November'),
+    (12, 'December')
+]
+
+class InputForm(FlaskForm):
+    month = SelectField(
+        'month',
+        choices=MONTHS,
+        validators=[DataRequired()]
+    )
+    year = IntegerField(
+        'year',
+        default=int(datetime.datetime.utcnow().year),
+        validators=[DataRequired()]
+    )
+    miles = IntegerField(
+        'miles',
+        validators=[DataRequired()]
+    )
+    avg_drivers = IntegerField(
+        'avg_drivers',
+        validators=[DataRequired()]
+    )
