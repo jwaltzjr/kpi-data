3d95bab9e3a6c8d31ee7c8ea6d4034555de02867
jwaltzjr <jwaltzjr@hotmail.com>
2017-06-16 14:13:06 -0500

    Fix krc import

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-from ..krc.database import DatabaseConnection
-from ..krc.env import EnvVar
+from krc.database import DatabaseConnection
+from krc.env import EnvVar
 
 app = Flask(__name__)
 app.config.from_object('config')
