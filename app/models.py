1a42d160317df6d3d694d362d9e3462ad737ab0c
jwaltzjr <jwaltzjr@hotmail.com>
2017-06-16 12:11:58 -0500

    Add MonthData model

---
+++ app/models.py
@@ -0,0 +1,9 @@
+class MonthData(object):
+
+    def __init__(self, month, year, miles, avg_drivers):
+        self.month = str(month) + str(year)
+        self.miles = miles
+        self.avg_drivers = avg_drivers
+
+    def __repr__(self):
+        return '<{} - {}, {}>'.format(self.month, self.miles, self.avg_drivers)
