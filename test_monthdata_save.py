005622de1d5b6d430c694621a8c117354e8063f0
jwaltzjr <jwaltzjr@hotmail.com>
2017-06-16 14:15:11 -0500

    print datab user/pass during test

--- test_monthdata_save.py
+++ test_monthdata_save.py
@@ -1,4 +1,7 @@
 from app.models import MonthData
+from app import DATABASE_USER, DATABASE_PASSWORD
 
+print(DATABASE_USER)
+print(DATABASE_PASSWORD)
 test_month = MonthData('04','2017',10000,65)
 test_month.save()
