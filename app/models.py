0bc88256307a6eb36faa680c21722bd8244aeeb9
jwaltzjr <jwaltzjr@hotmail.com>
2017-06-16 12:43:37 -0500

    Added a column for month as datetime

--- app/models.py
+++ app/models.py
@@ -1,9 +1,12 @@
+import datetime
+
 class MonthData(object):
 
     def __init__(self, month, year, miles, avg_drivers):
-        self.month = str(month) + str(year)
+        self.month_id = str(month) + '-' + str(year)
+        self.month_start = datetime.datetime.strptime(self.month_id, '%m-%Y')
         self.miles = miles
         self.avg_drivers = avg_drivers
 
     def __repr__(self):
-        return '<MONTHDATA {} - {}, {}>'.format(self.month, self.miles, self.avg_drivers)
+        return '<MONTHDATA {} - {}, {}>'.format(self.month_id, self.miles, self.avg_drivers)
